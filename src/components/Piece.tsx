096512f2c242020360ce978ace306942c5b39d95
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-28 15:26:34 -0700

    Added attr to display if piece can be moved or not.

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -4,19 +4,26 @@ import { Circle } from "@chakra-ui/react";
 interface PieceInterface {
   color: 0 | 1; // 0 for black, 1 for red
   visibile: true | false;
+  movable: true | false;
 }
 
-const Piece: React.FC<PieceInterface> = ({ color, visibile }) => {
+const Piece: React.FC<PieceInterface> = ({ color, visibile, movable }) => {
   const bg = color ? "red" : "gray.700";
   return (
     <Circle
-      size="85px"
-      bg={bg}
-      border="4px"
-      borderColor="black"
+      size="100px"
+      bg={movable ? "orange" : ""}
       visibility={visibile ? "visible" : "hidden"}
     >
-      <Circle size="55px" bg={bg} border="4px" borderColor="black" />
+      <Circle
+        size="85px"
+        bg={bg}
+        border="4px"
+        borderColor="black"
+        shadow="dark-lg"
+      >
+        <Circle size="55px" bg={bg} border="4px" borderColor="black" />
+      </Circle>
     </Circle>
   );
 };
