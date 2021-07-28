b5807d8b1ca1e52741068f51d8ed4ca65560092c
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-28 15:26:58 -0700

    Refactored.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Text, Box, Center, HStack, VStack } from "@chakra-ui/react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Piece from "./Piece";
-import isUnit from "../utils/distance";
+import validateMove from "../utils/validate";
 
 interface BoardProps {
   size: number;
@@ -23,20 +23,28 @@ const LIGHT = "#DBA65E";
 const OUTLINE = "#412B11";
 const TOPSPACING = 107.5;
 
-const Square: React.FC<SquareProps> = ({ row, col, size, token }) => {
+const BSquare: React.FC<SquareProps> = ({ row, col, size, token }) => {
   const offset = row % 2 ? 0 : 1;
   const bg = (col + offset) % 2 ? LIGHT : DARK;
   const one_d = row * size + col;
   return (
     <Center width="115px" height="115px" bg={bg}>
       {
-      <Draggable index={one_d} draggableId={String(one_d)} isDragDisabled={token <= 0}>
-        {({ draggableProps, dragHandleProps, innerRef }) => (
-          <Box {...draggableProps} {...dragHandleProps} ref={innerRef}>
-            <Piece color={token === 2 ? 1 : 0} visibile={token !== 0}/>
-          </Box>
-        )}
-      </Draggable>
+        <Draggable
+          index={one_d}
+          draggableId={String(one_d)}
+          isDragDisabled={token <= 0}
+        >
+          {({ draggableProps, dragHandleProps, innerRef }) => (
+            <Box {...draggableProps} {...dragHandleProps} ref={innerRef}>
+              <Piece
+                color={Math.abs(token) === 2 ? 1 : 0}
+                visibile={token !== 0}
+                movable={token > 0}
+              />
+            </Box>
+          )}
+        </Draggable>
       }
     </Center>
   );
@@ -50,11 +58,20 @@ const Row: React.FC<RowProps> = ({ row, size, tokens }) => {
         const one_d = row * size + idx;
         const isLight = (idx + (row % 2 ? 0 : 1)) % 2 ? true : false;
         return (
-          <Droppable droppableId={String(one_d)} key={one_d} isDropDisabled={isLight}>
+          <Droppable
+            droppableId={String(one_d)}
+            key={one_d}
+            isDropDisabled={tokens[idx] !== 0 || isLight}
+          >
             {({ droppableProps, innerRef }) => (
               <Box {...droppableProps} ref={innerRef}>
-                <Square row={row} col={idx} size={size} token={tokens[idx]}
-                  tokens={tokens}/>
+                <BSquare
+                  row={row}
+                  col={idx}
+                  size={size}
+                  token={tokens[idx]}
+                  tokens={tokens}
+                />
               </Box>
             )}
           </Droppable>
@@ -70,29 +87,30 @@ const Board: React.FC<BoardProps> = ({ size }) => {
   //Red: 2 Normal, 5 King
   //If Negative then disabled
   const [grid, setGrid] = useState([
-    0, 1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0,
+    0, -1, 0, -1, 0, -1, 0, -1,
+    -1, 0, -1, 0, -1, 0, -1, 0,
     0, 1, 0, 1, 0, 1, 0, 1,
 
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0,
 
-    2, 0, 2, 0, 2, 0, 2, 0,
-    0, 2, 0, 2, 0, 2, 0, 2,
-    2, 0, 2, 0, 2, 0, 2, 0,
+    -2, 0, -2, 0, -2, 0, -2, 0,
+    0, -2, 0, -2, 0, -2, 0, -2,
+    -2, 0, -2, 0, -2, 0, -2, 0,
   ]);
 
-  const handleOnDragEnd = (result: {[key: string]: any}) => {
+  const handleOnDragEnd = (result: { [key: string]: any }) => {
     if (!result.destination) return;
-    const source = parseInt(result.source.droppableId);
-    const destination = parseInt(result.destination.droppableId);
-    if (!isUnit(source, destination, size)) return;
+    console.log(result);
+    const src = parseInt(result.source.droppableId);
+    const dst = parseInt(result.destination.droppableId);
+    if (!validateMove(grid, size, src, dst)) return;
     const tempGrid = Array.from(grid);
-    const temp = tempGrid[source];
-    tempGrid[source] = tempGrid[destination];
-    tempGrid[destination] = temp;
+    const temp = tempGrid[src];
+    tempGrid[src] = tempGrid[dst];
+    tempGrid[dst] = temp;
     setGrid(tempGrid);
-  }
+  };
 
   return (
     <VStack spacing={0} bg={OUTLINE} rounded="2xl">
@@ -104,8 +122,13 @@ const Board: React.FC<BoardProps> = ({ size }) => {
       <DragDropContext onDragEnd={handleOnDragEnd}>
         {new Array(size).fill(0).map((_, idx) => {
           let rowStart = idx * size;
-          return <Row row={idx} size={size}
-                  tokens={grid.slice(rowStart, rowStart + size)}/>;
+          return (
+            <Row
+              row={idx}
+              size={size}
+              tokens={grid.slice(rowStart, rowStart + size)}
+            />
+          );
         })}
       </DragDropContext>
       <HStack spacing={TOPSPACING}>
