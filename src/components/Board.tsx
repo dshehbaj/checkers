519f7531a058c14cad8ca98d86c796b59d9bbf3c
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-28 04:09:30 -0700

    Integrated state management with drag and drop.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Text, Box, Center, HStack, VStack } from "@chakra-ui/react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Piece from "./Piece";
@@ -9,10 +9,12 @@ interface BoardProps {
 
 interface RowProps extends BoardProps {
   row: number;
+  tokens: number[];
 }
 
 interface SquareProps extends RowProps {
   col: number;
+  token: number;
 }
 
 const DARK = "#A05F18";
@@ -20,27 +22,26 @@ const LIGHT = "#DBA65E";
 const OUTLINE = "#412B11";
 const TOPSPACING = 90;
 
-const Square: React.FC<SquareProps> = ({ row, col, size }) => {
+const Square: React.FC<SquareProps> = ({ row, col, size, token }) => {
   const offset = row % 2 ? 0 : 1;
   const bg = (col + offset) % 2 ? LIGHT : DARK;
   const one_d = row * size + col;
-  const mid = Math.floor(size / 2);
   return (
     <Center width="100px" height="100px" bg={bg}>
-      {bg === DARK && row !== mid && row !== mid - 1 && (
-        <Draggable index={one_d} draggableId={String(one_d)}>
-          {({ draggableProps, dragHandleProps, innerRef }) => (
-            <Box {...draggableProps} {...dragHandleProps} ref={innerRef}>
-              <Piece color={row > mid ? 1 : 0} />
-            </Box>
-          )}
-        </Draggable>
-      )}
+      {token !== 0 &&
+      <Draggable index={one_d} draggableId={String(one_d)}>
+        {({ draggableProps, dragHandleProps, innerRef }) => (
+          <Box {...draggableProps} {...dragHandleProps} ref={innerRef}>
+            <Piece color={token === 2 ? 1 : 0} />
+          </Box>
+        )}
+      </Draggable>
+      }
     </Center>
   );
 };
 
-const Row: React.FC<RowProps> = ({ row, size }) => {
+const Row: React.FC<RowProps> = ({ row, size, tokens }) => {
   return (
     <HStack spacing={0}>
       <Text p={1.5}>{size - row}</Text>
@@ -50,7 +51,8 @@ const Row: React.FC<RowProps> = ({ row, size }) => {
           <Droppable droppableId={String(one_d)} key={one_d}>
             {({ droppableProps, innerRef }) => (
               <Box {...droppableProps} ref={innerRef}>
-                <Square row={row} col={idx} size={size} />
+                <Square row={row} col={idx} size={size} token={tokens[idx]}
+                  tokens={tokens}/>
               </Box>
             )}
           </Droppable>
@@ -62,6 +64,31 @@ const Row: React.FC<RowProps> = ({ row, size }) => {
 };
 
 const Board: React.FC<BoardProps> = ({ size }) => {
+  const [grid, setGrid] = useState([
+    0, 1, 0, 1, 0, 1, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 1,
+
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+
+    2, 0, 2, 0, 2, 0, 2, 0,
+    0, 2, 0, 2, 0, 2, 0, 2,
+    2, 0, 2, 0, 2, 0, 2, 0,
+  ]);
+
+  const handleOnDragEnd = (result: {[key: string]: any}) => {
+    if (!result.destination) return;
+    console.log(result);
+    const source = parseInt(result.source.droppableId);
+    const destination = parseInt(result.destination.droppableId);
+    const tempGrid = Array.from(grid);
+    const temp = tempGrid[source];
+    tempGrid[source] = tempGrid[destination];
+    tempGrid[destination] = temp;
+    setGrid(tempGrid);
+  }
+
   return (
     <VStack spacing={0} bg={OUTLINE} rounded="2xl">
       <HStack spacing={TOPSPACING}>
@@ -69,9 +96,11 @@ const Board: React.FC<BoardProps> = ({ size }) => {
           return <Text>{String.fromCharCode("a".charCodeAt(0) + idx)}</Text>;
         })}
       </HStack>
-      <DragDropContext onDragEnd={(res) => console.log(res)}>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
         {new Array(size).fill(0).map((_, idx) => {
-          return <Row row={idx} size={size} />;
+          let rowStart = idx * size;
+          return <Row row={idx} size={size}
+                  tokens={grid.slice(rowStart, rowStart + size)}/>;
         })}
       </DragDropContext>
       <HStack spacing={TOPSPACING}>
