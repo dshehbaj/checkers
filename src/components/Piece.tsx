9c236fca9f8f834470c055220dad5af82f8ec23d
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-28 14:12:24 -0700

    Added visibility option for Pieces.

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -3,12 +3,19 @@ import { Circle } from "@chakra-ui/react";
 
 interface PieceInterface {
   color: 0 | 1; // 0 for black, 1 for red
+  visibile: true | false;
 }
 
-const Piece: React.FC<PieceInterface> = ({ color }) => {
+const Piece: React.FC<PieceInterface> = ({ color, visibile }) => {
   const bg = color ? "red" : "gray.700";
   return (
-    <Circle size="85px" bg={bg} border="4px" borderColor="black">
+    <Circle
+      size="85px"
+      bg={bg}
+      border="4px"
+      borderColor="black"
+      visibility={visibile ? "visible" : "hidden"}
+    >
       <Circle size="55px" bg={bg} border="4px" borderColor="black" />
     </Circle>
   );
